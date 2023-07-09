b63711e1d2737d21eee98e12be614bfd146c3ea6
pinocchio <118585642+tp-7@users.noreply.github.com>
2023-07-09 14:04:25 -0400

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,17 +1,20 @@
-// gallery.js
-
 document.addEventListener('DOMContentLoaded', function () {
   const galleryContainer = document.querySelector('.gallery');
 
-  fetch('https://api.github.com/repos/tp-7/Gallery/contents/')
+  fetch('https://api.github.com/repos/tp-7/Gallery/contents')
     .then(response => response.json())
     .then(data => {
       data.forEach(item => {
-        if (item.type === 'file' && item.name.endsWith('.jpg')) {
+        if (item.type === 'file' && item.name.match(/\.(png|jpe?g|gif)$/)) {
           const imgElement = document.createElement('img');
           imgElement.src = item.download_url;
           imgElement.alt = 'Artwork';
-          galleryContainer.appendChild(imgElement);
+          
+          const galleryItem = document.createElement('div');
+          galleryItem.className = 'gallery-item';
+          galleryItem.appendChild(imgElement);
+          
+          galleryContainer.appendChild(galleryItem);
         }
       });
     })
